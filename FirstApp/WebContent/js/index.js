de2413decbc9a58d0f2db5f0671c8228ea2c4c39
Rajamohan Raju <Rajamohan Raju@RAJAMOHAN>
2017-05-08 08:25:29 +0530

    Added JS file for login validation

--- FirstApp/WebContent/js/index.js
+++ FirstApp/WebContent/js/index.js
@@ -1,5 +1,20 @@
-/**
- * 
- */
+function validateLogin() {
 
-//The JS functionality goes here.
+	var name = document.forms["loginForm"]["username"].value;
+	if (name == "") {
+		alert("Please enter User Name");
+		return false;
+	} else if (name == "kishore") {
+
+	}
+
+}
+
+function check(form) {
+	if (form.username.value == "raju" && form.password.value == "raju") {
+		window.location.href = 'http://localhost:8080/FirstApp/success.html';
+		return false;
+	} else {
+		alert("Error Password or Username");
+	}
+}
