a7938fdb1e1c29a6a97076bb364ebf0e7b26a787
Rajamohan Raju <Rajamohan Raju@RAJAMOHAN>
2017-05-30 22:33:35 +0530

    Login Functionality with href has done.

--- FirstApp/WebContent/js/index.js
+++ FirstApp/WebContent/js/index.js
@@ -1,26 +1,8 @@
-function validateLogin() {
 
-	var name = document.forms["loginForm"]["username"].value;
-	if (name == "") {
-		alert("Please enter User Name");
+function login(loginForm){
+	
+	if(loginForm.username.value == "kishore" && loginForm.password.value == "kishore"){
+		window.location.href = 'success.html';
 		return false;
-	} else if (name == "kishore") {
-
 	}
-
-}
-
-function login(form){
-	
-	if (form.username.value == "raju" && form.password.value == "raju") {
-		_redirect();
-	} else {
-		alert("Error Password or Username");
-	}
-	
-}
-
-function _redirect() {
-	window.location.assign("http://localhost:8080/FirstApp/success.html");
-	return false;
 }
